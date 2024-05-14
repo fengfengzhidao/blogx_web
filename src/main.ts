4615c0776fc42dd937f3bbe11fcd713d85b53e30
fengfeng <fengfengzhidao@126.com>
2024-05-14 22:22:35 +0800

    admin结构-html结构

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,13 @@ import "@/assets/base.css"
 import App from './App.vue'
 import router from './router'
 import ArcoVue from '@arco-design/web-vue';
+import ArcoVueIcon from '@arco-design/web-vue/es/icon';
 import '@arco-design/web-vue/dist/arco.css';
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(ArcoVue)
+app.use(ArcoVueIcon)
 
 app.mount('#app')
--- src/views/admin/index.vue
+++ src/views/admin/index.vue
@@ -3,9 +3,39 @@
 </script>
 
 <template>
-  <div>admin</div>
+  <div class="f_admin">
+    <div class="f_aside">
+      <div class="f_logo">
+
+      </div>
+      <div class="f_menu">
+
+      </div>
+    </div>
+    <div class="f_main">
+      <div class="f_head">
+        <div class="f_breadcrumbs"></div>
+        <div class="f_actions">
+          <icon-home />
+          <icon-sun-fill />
+          <icon-moon-fill />
+          <icon-fullscreen />
+          <icon-fullscreen-exit />
+          <div class="f_user_info_action">
+
+          </div>
+        </div>
+      </div>
+      <div class="f_tabs">
+
+      </div>
+      <div class="f_container">
+        <router-view></router-view>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped>
+<style lang="less">
 
 </style>
