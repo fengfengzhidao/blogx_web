1d4cc529cffdfb9d3338e5f2017f42a61b46fbb3
fengfeng <fengfengzhidao@126.com>
2024-05-15 22:54:49 +0800

    小按钮定位

--- src/components/admin/f_menu.vue
+++ src/components/admin/f_menu.vue
@@ -6,61 +6,96 @@ import F_component from "@/components/common/f_component.vue";
 interface MenuType {
   title: string
   name: string
-  icon?: string|Component
+  icon?: string | Component
   children?: MenuType[]
 }
 
 
-const menuList : MenuType[] = [
+const menuList: MenuType[] = [
   {title: "首页", name: "admin", icon: IconHome},
-  {title: "个人中心", name: "userCenter", icon: IconUser, children: [
+  {
+    title: "个人中心", name: "userCenter", icon: IconUser, children: [
       {title: "用户信息", name: "userInfo",}
-    ]},
-  {title: "用户管理", name: "userManage", icon: IconUser, children: [
+    ]
+  },
+  {
+    title: "用户管理", name: "userManage", icon: IconUser, children: [
       {title: "用户列表", name: "userList",}
-    ]},
-  {title: "组件管理", name: "componentManage", icon: "iconfont xxx", children: [
+    ]
+  },
+  {
+    title: "组件管理", name: "componentManage", icon: "iconfont xxx", children: [
       {title: "组件列表", name: "componentList",}
-    ]},
-  {title: "系统设置", name: "settingsManage", icon: IconSettings, children: [
+    ]
+  },
+  {
+    title: "系统设置", name: "settingsManage", icon: IconSettings, children: [
       {title: "系统信息", name: "settingsInfo",}
-    ]},
+    ]
+  },
 ]
 
 
 </script>
 
 <template>
-<div class="f_menu scrollbar">
-  <a-menu
-      show-collapse-button>
-    <template v-for="menu in menuList">
-      <a-menu-item :key="menu.name" v-if="!menu.children">
-        <template #icon>
-          <f_component :is="menu.icon"></f_component>
+  <div class="f_menu">
+    <div class="f_menu_inner scrollbar">
+      <a-menu
+          show-collapse-button>
+        <template v-for="menu in menuList">
+          <a-menu-item :key="menu.name" v-if="!menu.children">
+            <template #icon>
+              <f_component :is="menu.icon"></f_component>
+            </template>
+            {{ menu.title }}
+          </a-menu-item>
+          <a-sub-menu :key="menu.name" v-else :title="menu.title">
+            <template #icon>
+              <f_component :is="menu.icon"></f_component>
+            </template>
+            <a-menu-item :key="sub.name" v-for="sub in menu.children">
+              {{ sub.title }}
+              <template #icon>
+                <f_component :is="menu.icon"></f_component>
+              </template>
+            </a-menu-item>
+          </a-sub-menu>
         </template>
-        {{ menu.title }}
-      </a-menu-item>
-      <a-sub-menu :key="menu.name" v-else :title="menu.title">
-        <template #icon>
-          <f_component :is="menu.icon"></f_component>
-        </template>
-        <a-menu-item :key="sub.name" v-for="sub in menu.children">
-          {{ sub.title }}
-          <template #icon>
-            <f_component :is="menu.icon"></f_component>
-          </template>
-        </a-menu-item>
-      </a-sub-menu>
-    </template>
-  </a-menu>
-</div>
+      </a-menu>
+    </div>
+
+  </div>
 </template>
 
 <style lang="less">
-.f_menu{
+.f_menu {
   height: calc(100vh - 90px);
-  overflow-y: auto;
-  overflow-x: hidden;
+  position: relative;
+
+  &:hover{
+    .arco-menu-collapse-button {
+      opacity: 1 !important;
+    }
+  }
+
+  .f_menu_inner {
+    height: 100%;
+    overflow-y: auto;
+    overflow-x: hidden;
+
+    .arco-menu {
+      position: inherit;
+      height: 100%;
+
+      .arco-menu-collapse-button {
+        top: 50%;
+        transform: translate(-50%, -50%);
+        left: 240px;
+        transition: all .3s;
+        opacity: 0;
+      }
+    }
+  }
 }
 </style>
