a47a10bd20ffa0051e72dd91aff980b01c755403
fengfeng <fengfengzhidao@126.com>
2024-05-16 00:47:59 +0800

    tabs组件-删除单个

--- src/components/admin/f_tabs.vue
+++ src/components/admin/f_tabs.vue
@@ -2,6 +2,7 @@
 import {IconClose} from "@arco-design/web-vue/es/icon";
 import {useRoute} from "vue-router";
 import router from "@/router";
+import {ref} from "vue";
 
 const route = useRoute()
 
@@ -10,17 +11,31 @@ interface TabType {
   title: string
 }
 
-const tabs: TabType[] = [
+const tabs = ref<TabType[]>([
   {title: "首页", name: "home"},
   {title: "个人信息", name: "userInfo"},
   {title: "用户列表", name: "userList"},
   {title: "系统信息", name: "settings"},
-]
+])
 
 function check(item: TabType) {
   router.push({
     name: item.name,
   })
+}
+
+function removeItem(item: TabType) {
+  const index = tabs.value.findIndex((value) => item.name === value.name)
+  if (index !== -1) {
+    // 判断我删除的这个元素，是不是就是我当前所在
+    if (item.name === route.name) {
+      // 因为首页那个item是100%不会被删除的，所以放心往前走
+      router.push({
+        name: tabs.value[index - 1].name
+      })
+    }
+    tabs.value.splice(index, 1)
+  }
 }
 
 
@@ -30,7 +45,7 @@ function check(item: TabType) {
   <div class="f_tabs">
     <div class="item" @click="check(item)" :class="{active: route.name === item.name}" v-for="item in tabs">
       {{ item.title }}
-      <span class="close" title="删除" v-if="item.name !== 'home'">
+      <span class="close" @click.stop="removeItem(item)" title="删除" v-if="item.name !== 'home'">
           <IconClose></IconClose>
         </span>
     </div>
