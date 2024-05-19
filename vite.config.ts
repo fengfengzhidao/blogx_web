ff639d3d93d0792c920e46c6f83f4f6a42ba4d50
fengfeng <fengfengzhidao@126.com>
2024-05-19 10:57:43 +0800

    在viteconfig中读取环境变量

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,37 @@
-import { fileURLToPath, URL } from 'node:url'
+import {fileURLToPath, URL} from 'node:url'
 
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import {loadEnv} from "vite";
+import type {EnvMeta} from "./env";
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  css:{
-    preprocessorOptions:{
-      less: {
-        modifyVars: {
-          // 'primary-6': "red",
+const envDir = "./" // env文件的目录
+export default defineConfig((config) => {
+    const env = loadEnv(config.mode, envDir) as EnvMeta
+    console.log(env.VITE_SERVER_URL)
+
+    return {
+        plugins: [vue()],
+        css: {
+            preprocessorOptions: {
+                less: {
+                    modifyVars: {
+                        // 'primary-6': "red",
+                    },
+                    additionalData: '@import "@/assets/var.less";',
+                    javascriptEnabled: true,
+                }
+            }
         },
-        additionalData: '@import "@/assets/var.less";',
-        javascriptEnabled: true,
-      }
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url))
+            }
+        },
+        server: {
+            host: "0.0.0.0",
+            port: 80
+        },
+        envDir: envDir,
     }
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  envDir: "./"
 })
