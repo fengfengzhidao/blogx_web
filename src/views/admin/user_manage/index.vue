a8b675989bd4273544a362197986ad0adcb03dd4
fengfeng <fengfengzhidao@126.com>
2024-05-25 17:46:25 +0800

    接收emits返回值

--- src/views/admin/user_manage/index.vue
+++ src/views/admin/user_manage/index.vue
@@ -4,7 +4,7 @@ import F_list, {type filterGroupType} from "@/components/admin/f_list.vue";
 import {userListApi, type userListType} from "@/api/user_api";
 import type {columnType} from "@/components/admin/f_list.vue";
 import {reactive, ref} from "vue";
-import F_modal_form from "@/components/admin/f_modal_form.vue";
+import F_modal_form, {type formListType} from "@/components/admin/f_modal_form.vue";
 
 const columns = [
   {title: "ID", dataIndex: 'id'},
@@ -15,29 +15,24 @@ const columns = [
   // {title: "操作", slotName: 'action'},
 ]
 
-const formList = [
+const formList: formListType[] = [
   {
-    label: "昵称", field: "nick_name",  type: "input",
+    label: "昵称", field: "nick_name", type: "input", rules: {required: true}, validateTrigger: "blur"
   }
 ]
 const form = reactive({})
 const visible = ref(false)
 
+function ok(form: object, fn: (val: boolean) => void) {
+  // console.log(form)
+  // fn(false)
+}
 </script>
 
 <template>
   <div>
-    <f_modal_form v-model:visible="visible"></f_modal_form>
-    <a-modal v-model:visible="visible" title="创建用户">
-      <a-form :model="form">
-        <a-form-item v-for="item in formList" :field="item.field" :label="item.label" :rules="{required: true}" validate-trigger="blur">
-          <template v-if="item.type === 'input'">
-            <a-input v-model="form[item.field]" :placeholder="item.label"></a-input>
-          </template>
-        </a-form-item>
-      </a-form>
-    </a-modal>
-    <f_list @add="visible=true" ref="fListRef"  :url="userListApi" :columns="columns">
+    <f_modal_form @ok="ok" v-model:visible="visible" title="创建用户" :form-list="formList"></f_modal_form>
+    <f_list @add="visible=true" ref="fListRef" :url="userListApi" :columns="columns">
       <template #avatar="{record}:{record: userListType}">
         <a-avatar :image-url="record.avatar"></a-avatar>
       </template>
