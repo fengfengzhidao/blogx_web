7c61f6d2108287a5a9607bbd11ddf82dbb06ed8a
fengfeng <fengfengzhidao@126.com>
2024-05-26 11:59:14 +0800

    mock的一些修改

--- src/mock/user_mock.ts
+++ src/mock/user_mock.ts
@@ -4,21 +4,21 @@ export function userMock(){
     mock(/.*?\/api\/email_login/, function (options: MockjsRequestOptions){
         return {
             code: 0,
-            data: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwibmlja19uYW1lIjoi5p6r5p6rIiwicm9sZSI6MSwidXNlcl9pZCI6MSwiZXhwIjoxNzE2Mzc4NTI1LjcyMDk4MiwiaXNzIjoiMTIzNCJ9.48zvrEbJ6KkUVIL8ivhdDQStm7d5FqhuZ7nf0xO-ydg",
+            data: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwibmlja19uYW1lIjoi5p6r5p6rIiwicm9sZSI6MSwidXNlcl9pZCI6MSwiZXhwIjoyNTgwNjk1NjE1Ljc1NDg1LCJpc3MiOiIxMjM0In0.791XW8rCtwDqtyiLT8-RjCmbjdvZtqkc2Jm_W7wd-As",
             msg: "成功"
         }
     })
 
 
-    mock("/.*?\/api/user_info/", function (options: MockjsRequestOptions){
+    mock(/.*?\/api\/user_info/, function (options: MockjsRequestOptions){
         return {
             "code": 0,
             "data": {
                 "id": 1,
                 "created_at": "2023-02-19T17:23:04.543+08:00",
-                "nick_name": "枫枫",
-                "user_name": "admin",
-                "avatar": "/uploads/file/枫枫/images_20231005232530.png",
+                "nick_name": "@cname",
+                "user_name": "@name",
+                "avatar": "@img(100x100)",
                 "email": "2663456523@qq.com",
                 "tel": "",
                 "addr": "内网地址",
@@ -32,5 +32,35 @@ export function userMock(){
             },
             "msg": "成功"
         }
+    })
+
+    mock(/.*?\/api\/users/, function (){
+        return mock({
+            code: 0,
+            msg: "成功",
+            data: {
+                count: 4,
+                "list|3-10": [
+                    {
+                        "id": 27,
+                        "created_at": "2024-05-25T22:01:41.184+08:00",
+                        "nick_name": "@cname",
+                        "user_name": "@name",
+                        "avatar": "@img(100x100)",
+                        "email": "",
+                        "tel": "",
+                        "addr": "内网地址",
+                        "token": "",
+                        "ip": "127.0.0.1",
+                        "role": "用户",
+                        "sign_status": "邮箱",
+                        "integral": 0,
+                        "sign": "",
+                        "link": "",
+                        "role_id": 2
+                    },
+                ]
+            }
+        })
     })
 }
