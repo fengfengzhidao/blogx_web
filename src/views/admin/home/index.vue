ae36cb33243b37dac5ff06152bd09219a32d72f0
fengfeng <fengfengzhidao@126.com>
2024-05-26 12:14:45 +0800

    后台首页整体布局搭建

--- src/views/admin/home/index.vue
+++ src/views/admin/home/index.vue
@@ -1,11 +1,56 @@
 <script setup lang="ts">
 
+import F_card from "@/components/common/f_card.vue";
 </script>
 
 <template>
-  <div>Home</div>
+  <div class="home_view">
+    <div class="welcome">
+
+    </div>
+    <div class="bottom">
+      <div class="left">
+        <f_card title="快捷入口" class="quick_room">
+
+        </f_card>
+        <f_card title="数据统计" class="statistics">
+
+        </f_card>
+      </div>
+      <div class="right">
+        <f_card title="更新日志" class="version">
+
+        </f_card>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped>
+<style lang="less">
+.home_view {
+  background-color: inherit !important;
 
+  .welcome {
+    height: 251px;
+    width: 100%;
+    background-color: var(--color-bg-1);
+    border-radius: 5px;
+    margin-bottom: 20px;
+  }
+
+  .bottom{
+    display: flex;
+    justify-content: space-between;
+    .left{
+      width: 70%;
+      .statistics{
+        margin-top: 20px;
+      }
+    }
+    .right{
+      width: 30%;
+      margin-left: 20px;
+    }
+  }
+}
 </style>
