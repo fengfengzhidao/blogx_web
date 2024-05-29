6ef994d72fbdaa471b54dd935b1ce7cff70d074e
fengfeng <fengfengzhidao@126.com>
2024-05-29 23:52:06 +0800

    底部信息栏

--- src/conf/global.ts
+++ src/conf/global.ts
@@ -2,4 +2,6 @@ export const title = "枫枫知道后台管理系统1"
 export const slogan = "枫枫知道"
 export const enSlogan = "fengfengzhidao"
 export const ico = "/logo.png"
+
+export const beian = "湘ICP备2021010654号-3"
 // export const ico = "https://fanyi-cdn.cdn.bcebos.com/webStatic/translation/img/favicon/favicon-32x32.png"
--- src/views/web/web_home.vue
+++ src/views/web/web_home.vue
@@ -1,13 +1,19 @@
 <script setup lang="ts">
 
 import F_nav from "@/components/web/f_nav.vue";
+import F_footer from "@/components/web/f_footer.vue";
 </script>
 
 <template>
-<div style="height: 200vh; background-color: #2a2a2b">
+  <div>
 
-  <f_nav ></f_nav>
-  首页</div>
+    <f_nav></f_nav>
+    <div style="height: 200vh; background-color: #2a2a2b">
+
+    </div>
+    <f_footer></f_footer>
+
+  </div>
 </template>
 
 <style scoped lang="less">
