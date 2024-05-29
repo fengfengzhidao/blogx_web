ada896a1adb8d70171342dd363a359bdea163b07
fengfeng <fengfengzhidao@126.com>
2024-05-29 23:59:51 +0800

    默认布局组件

--- src/views/web/web_home.vue
+++ src/views/web/web_home.vue
@@ -2,17 +2,16 @@
 
 import F_nav from "@/components/web/f_nav.vue";
 import F_footer from "@/components/web/f_footer.vue";
+import F_main from "@/components/web/f_main.vue";
 </script>
 
 <template>
-  <div>
-
-    <f_nav></f_nav>
-    <div style="height: 200vh; background-color: #2a2a2b">
-
-    </div>
+  <div class="web_home_view">
+    <f_nav no-scroll></f_nav>
+    <f_main>
+      首页
+    </f_main>
     <f_footer></f_footer>
-
   </div>
 </template>
 
