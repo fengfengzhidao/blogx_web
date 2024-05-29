94899cb25cfa7590f6f5d9976c7c4d12b4646473
fengfeng <fengfengzhidao@126.com>
2024-05-29 22:30:41 +0800

    js修改网页标题和图标

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,20 @@
 <script setup lang="ts">
 import {userStorei} from "@/stores/user_store";
+import {title, ico} from "@/conf/global";
 
 const store = userStorei()
 store.loadUserInfo()
+
+if (title){
+  document.title = title
+}
+if (ico){
+  const link = document.querySelector('link[rel="icon"]')
+  if (link){
+    link.setAttribute("href", ico)
+  }
+}
+
 </script>
 
 <template>
