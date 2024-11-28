1f8e5ba10615d8c1a2fdaf207db4a2aa2d8b79af
fengfeng <2663456523@qq.com>
2024-11-28 19:53:57 +0800

    站点配置路由

---
+++ src/views/admin/settings_manage/site_manage/email.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>邮箱设置</div>
+</template>
+
+<style scoped lang="less">
+
+</style>
---
+++ src/views/admin/settings_manage/site_manage/site.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div>网站设置</div>
+</template>
+
+<style scoped lang="less">
+
+</style>
