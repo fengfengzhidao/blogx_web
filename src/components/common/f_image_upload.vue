7f894142f8fd97cbc663fb25e362868042e7b96f
fengfeng <2663456523@qq.com>
2024-11-28 21:55:27 +0800

    站点配置-图片上传足迹

--- src/components/common/f_image_upload.vue
+++ src/components/common/f_image_upload.vue
@@ -1,13 +1,29 @@
 <script setup lang="ts">
+import {userStorei} from "@/stores/user_store";
+import {type FileItem, Message} from "@arco-design/web-vue";
+import type {baseResponse} from "@/api";
+
+const store = userStorei()
 interface Props {
   modelValue: string
   placeholder?: string
 }
+
 const props = defineProps<Props>()
 const emits = defineEmits(["update:modelValue"])
 
-function inputHandler(val: string){
+function inputHandler(val: string) {
   emits("update:modelValue", val)
+}
+
+function fileUploadCallback(file: FileItem){
+  const res = JSON.parse(file.response) as baseResponse<string>
+  if (res.code){
+    Message.error(res.msg)
+    return
+  }
+  Message.success(res.msg)
+  emits("update:modelValue", res.data)
 }
 
 </script>
@@ -15,10 +31,20 @@ function inputHandler(val: string){
 <template>
   <div class="f_image_upload">
     <a-input :model-value="props.modelValue" :placeholder="props.placeholder" @input="inputHandler"></a-input>
-    <a-upload></a-upload>
+    <a-upload :show-file-list="false" action="/api/images" @success="fileUploadCallback" name="file" :headers="{token: store.userInfo.token}">
+      <template #upload-button>
+        <a-avatar :image-url="props.modelValue" :size="60" ></a-avatar>
+      </template>
+    </a-upload>
   </div>
 </template>
 
-<style scoped lang="less">
-
+<style lang="less">
+.f_image_upload {
+  width: 100%;
+  .arco-input-wrapper{
+    display: flex;
+    margin-bottom: 10px;
+  }
+}
 </style>
