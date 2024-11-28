47d74c60be3b7342b599238eacfaaf053223e912
fengfeng <2663456523@qq.com>
2024-11-28 23:55:39 +0800

    teleport传送组件

--- src/views/admin/settings_manage/site_manage/email.vue
+++ src/views/admin/settings_manage/site_manage/email.vue
@@ -1,16 +1,26 @@
 <script setup lang="ts">
 
 import F_site from "@/components/admin/site/f_site.vue";
+import F_title from "@/components/common/f_title.vue";
 </script>
 
 <template>
   <div class="email_view">
-    <f_site name="email" v-slot="{data}">
-      {{ data }}
+    <f_site name="email" v-slot="{form}">
+      <f_title>邮箱设置</f_title>
+      <div class="body">
+        <a-alert type="info">配置邮箱之后，系统的邮箱注册功能才能正常使用</a-alert>
+        <a-form>
+          <a-form-item label="邮箱域名">
+
+          </a-form-item>
+          <div class="site_update_btn"></div>
+        </a-form>
+      </div>
     </f_site>
   </div>
 </template>
 
-<style scoped lang="less">
+<style lang="less">
 
 </style>
