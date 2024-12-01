9531ea10dbc20b1e1c76f4d2ab742d7038d02cd7
fengfeng <2663456523@qq.com>
2024-12-01 20:53:55 +0800

    数据统计页-页面调整

--- src/views/admin/home/index.vue
+++ src/views/admin/home/index.vue
@@ -5,21 +5,36 @@ import F_welcome from "@/components/admin/f_welcome.vue";
 import F_quick_entrance from "@/components/admin/f_quick_entrance.vue";
 import F_version from "@/components/common/f_version.vue";
 import User_login_echarts from "@/components/echarts/user_login_echarts.vue";
+import F_title from "@/components/common/f_title.vue";
 </script>
 
 <template>
   <div class="home_view">
-    <f_welcome></f_welcome>
-    <div class="bottom">
-      <div class="left">
-        <f_quick_entrance></f_quick_entrance>
-        <f_card title="数据统计" class="statistics">
-<!--          <user_login_echarts></user_login_echarts>-->
-        </f_card>
+    <div class="left">
+      <f_welcome></f_welcome>
+      <div class="charts charts_3">
+        <div class="item">
+          <f_title :type="2">访问人数</f_title>
+        </div>
+        <div class="item">
+          <f_title :type="2">发布文章</f_title>
+        </div>
+        <div class="item">
+          <f_title :type="2">新增用户</f_title>
+        </div>
       </div>
-      <div class="right">
-        <f_version></f_version>
+      <div class="charts charts_2">
+        <div class="item">
+          <f_title :type="2">发布文章</f_title>
+        </div>
+        <div class="item">
+          <f_title :type="2">系统资源</f_title>
+        </div>
       </div>
+    </div>
+    <div class="right">
+      <f_quick_entrance></f_quick_entrance>
+      <f_version></f_version>
     </div>
   </div>
 </template>
@@ -27,23 +42,39 @@ import User_login_echarts from "@/components/echarts/user_login_echarts.vue";
 <style lang="less">
 .home_view {
   background-color: inherit !important;
+  display: flex;
+  justify-content: space-between;
 
+  .left {
+    width: 75%;
 
-  .bottom {
-    display: flex;
-    justify-content: space-between;
+    .charts {
+      margin-bottom: 20px;
+      grid-column-gap: 20px;
+      display: grid;
 
-    .left {
-      width: 70%;
-
-      .statistics {
-        margin-top: 20px;
+      .item {
+        background: var(--color-bg-1);
+        border-radius: 5px;
+        padding: 20px 10px;
       }
     }
 
-    .right {
-      width: 30%;
-      margin-left: 20px;
+    .charts_3 {
+      grid-template-columns: repeat(3, 1fr);
+    }
+
+    .charts_2 {
+      grid-template-columns: 7fr 3fr;
+    }
+
+  }
+
+  .right {
+    width: calc(25% - 20px);
+
+    .f_quick_entrance {
+      margin-bottom: 20px;
     }
   }
 }
