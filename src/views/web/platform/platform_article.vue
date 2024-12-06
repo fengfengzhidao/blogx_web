ff6a82e2cc4f0dcfdcb9cf2261c05afa149ecea0
fengfeng <2663456523@qq.com>
2024-12-06 19:43:10 +0800

    文章列表页面

--- src/views/web/platform/platform_article.vue
+++ src/views/web/platform/platform_article.vue
@@ -1,6 +1,33 @@
 <script setup lang="ts">
-
 import F_a from "@/components/common/f_a.vue";
+import {reactive} from "vue";
+import type {listResponse} from "@/api";
+import {articleListApi, type articleListRequest, type articleListType} from "@/api/article_api";
+import {Message} from "@arco-design/web-vue";
+import {IconEye, IconMessage} from "@arco-design/web-vue/es/icon";
+import {dateCurrentFormat} from "../../../utils/date";
+
+const data = reactive<listResponse<articleListType>>({
+  list: [],
+  count: 0
+})
+
+const params = reactive<articleListRequest>({
+  type: 2,
+})
+
+async function getData() {
+  const res = await articleListApi(params)
+  if (res.code) {
+    Message.error(res.msg)
+    return
+  }
+  Object.assign(data, res.data)
+}
+
+getData()
+
+
 </script>
 
 <template>
@@ -19,6 +46,31 @@ import F_a from "@/components/common/f_a.vue";
         <f_a>已发布</f_a>
         <f_a>审核中</f_a>
         <f_a>草稿箱</f_a>
+      </div>
+      <div class="article_list">
+        <div class="item" v-for="item in data.list">
+          <div class="cover">
+            <img v-if="item.cover" :src="item.cover" alt="">
+          </div>
+          <div class="info">
+            <div class="title">{{ item.title }}</div>
+            <div class="abs">{{ item.abstract }}</div>
+            <div class="data">
+              <div class="look">
+                <IconEye></IconEye>
+                <span>{{ item.lookCount }}</span>
+              </div>
+              <div class="comment">
+                <IconMessage></IconMessage>
+                <span>{{ item.commentCount }}</span>
+              </div>
+              <div class="tags">
+                <a-tag v-for="tag in item.tagList">{{ tag }}</a-tag>
+              </div>
+              <div class="date">最后更新于{{ dateCurrentFormat(item.updatedAt) }}</div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -67,6 +119,63 @@ import F_a from "@/components/common/f_a.vue";
 
         &:last-child {
           margin-right: 0;
+        }
+      }
+    }
+
+    .article_list {
+      margin-top: 10px;
+
+      .item {
+        display: flex;
+        margin-bottom: 10px;
+
+        .cover {
+          img {
+            width: 160px;
+            border-radius: 5px;
+            margin-right: 10px;
+          }
+        }
+
+        .info {
+          display: flex;
+          flex-direction: column;
+          justify-content: space-between;
+
+          .title {
+            font-size: 15px;
+            font-weight: 600;
+            color: var(--color-text-1);
+          }
+
+          .abs {
+            height: 2rem;
+            margin: 5px 0;
+          }
+
+          .data {
+            display: flex;
+            align-items: center;
+            color: var(--color-text-2);
+
+            .look, .comment {
+              margin-right: 10px;
+              span{
+                margin-left: 5px;
+              }
+            }
+            .tags{
+              margin-right: 10px;
+              .arco-tag{
+                margin-right: 5px;
+              }
+            }
+            .date{
+              font-size: 12px;
+              color: var(--color-text-2);
+            }
+          }
         }
       }
     }
