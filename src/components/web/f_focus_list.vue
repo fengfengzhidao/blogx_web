d04869926189f56162c18c0b08f2c1ece66567d8
fengfeng <2663456523@qq.com>
2024-12-10 21:57:04 +0800

    关注列表和粉丝列表完成

---
+++ src/components/web/f_focus_list.vue
@@ -0,0 +1,131 @@
+<script setup lang="ts">
+import {fansListApi, type fansListRequest, focusListApi, type userListType} from "@/api/focus_api";
+import {reactive} from "vue";
+import type {listResponse} from "@/api";
+import {userStorei} from "@/stores/user_store";
+import {Message} from "@arco-design/web-vue";
+import F_label from "@/components/common/f_label.vue";
+import {relationOptions} from "@/options/options";
+import type {baseResponse} from "@/api";
+
+interface Props {
+  userId: number
+  type: "focus" | "fans"
+}
+
+const props = defineProps<Props>()
+
+const store = userStorei()
+const data = reactive<listResponse<userListType>>({
+  list: [],
+  count: 0
+})
+const params = reactive<fansListRequest>({
+  userID: props.userId,
+  isMe: false,
+  limit: 10,
+  page: 1
+})
+
+async function getData() {
+  params.isMe = store.userInfo.userID == props.userId
+  let res: baseResponse<listResponse<userListType>>
+  if (props.type === "focus") {
+    res = await focusListApi(params)
+  } else {
+    res = await fansListApi(params)
+  }
+  if (res.code) {
+    Message.error(res.msg)
+    return
+  }
+  data.list = res.data.list
+  data.count = res.data.count
+}
+
+getData()
+
+</script>
+
+<template>
+  <div class="f_focus_list_com">
+    <div class="user_list">
+      <div class="item" v-for="item in data.list">
+        <a-avatar :size="60" :image-url="item.userAvatar">{{ item.userNickname[0] }}</a-avatar>
+        <div class="info">
+          <div class="nick">{{ item.userNickname }}
+
+            <f_label v-if="item.relationship !== 1" :options="relationOptions" :value="item.relationship"></f_label>
+          </div>
+          <div class="abs">
+            <a-typography-text :ellipsis="{rows: 1, css: true}">{{ item.userAbstract }}</a-typography-text>
+          </div>
+          <div class="action">
+            <a-button v-if="item.relationship === 2 || item.relationship === 4" type="primary" size="mini">已关注
+            </a-button>
+            <a-button v-else type="outline" size="mini">关注</a-button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="no_data" v-if="data.list.length === 0">
+      <a-empty></a-empty>
+    </div>
+    <div class="page" v-if="data.list.length">
+      <a-pagination :page-size="params.limit" v-model:current="params.page" :total="data.count" show-total
+                    @change="getData"></a-pagination>
+    </div>
+  </div>
+</template>
+
+<style lang="less">
+.f_focus_list_com {
+  padding: 20px;
+
+  .user_list {
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    grid-column-gap: 20px;
+    grid-row-gap: 20px;
+    width: 100%;
+
+
+    .item {
+      width: 100%;
+      display: flex;
+
+      .arco-avatar-circle {
+        flex-shrink: 0;
+      }
+
+      .info {
+        margin-left: 10px;
+
+        .nick {
+          height: 1.5rem;
+          display: flex;
+          align-items: center;
+        }
+
+        .arco-tag {
+          margin-left: 10px;
+          transform: scale(0.9);
+        }
+
+        .abs {
+          height: 1.5rem;
+        }
+      }
+
+    }
+
+  }
+
+  .page {
+    display: flex;
+    justify-content: center;
+    margin-top: 20px;
+  }
+
+}
+</style>
--- src/views/web/user/focus_list.vue
+++ src/views/web/user/focus_list.vue
@@ -1,103 +1,15 @@
 <script setup lang="ts">
-import {type fansListRequest, focusListApi, type userListType} from "@/api/focus_api";
-import {reactive} from "vue";
-import type {listResponse} from "@/api";
+import F_focus_list from "@/components/web/f_focus_list.vue";
 import {useRoute} from "vue-router";
-import router from "@/router";
-import {userStorei} from "@/stores/user_store";
-import {Message} from "@arco-design/web-vue";
 
-const store = userStorei()
 const route = useRoute()
-const data = reactive<listResponse<userListType>>({
-  list: [],
-  count: 0
-})
-const params = reactive<fansListRequest>({
-  userID: Number(route.params.id),
-  isMe: false,
-  limit: 10,
-  page: 1
-})
-
-async function getData() {
-  params.isMe = store.userInfo.userID == Number(route.params.id)
-  const res = await focusListApi(params)
-  if (res.code) {
-    Message.error(res.msg)
-    return
-  }
-  data.list = res.data.list
-  data.count = res.data.count
-}
-
-getData()
-
 
 </script>
 
 <template>
-  <div class="focus_list_view">
-    <div class="user_list">
-      <div class="item" v-for="item in data.list">
-        <a-avatar :size="60" :image-url="item.userAvatar">{{ item.userNickname[0] }}</a-avatar>
-        <div class="info">
-          <div class="nick">{{ item.userNickname }}</div>
-          <div class="abs">
-            <a-typography-text :ellipsis="{rows: 1, css: true}">{{ item.userAbstract }}</a-typography-text>
-          </div>
-          <div class="action">
-            <a-button type="outline" size="mini">关注</a-button>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="no_data" v-if="data.list.length === 0">
-      <a-empty></a-empty>
-    </div>
-    <div class="page" v-if="data.list.length">
-      <a-pagination :page-size="params.limit" v-model:current="params.page" :total="data.count" show-total
-                    @change="getData"></a-pagination>
-    </div>
-  </div>
+  <f_focus_list :user-id="Number(route.params.id)" type="focus"></f_focus_list>
 </template>
 
 <style lang="less">
-.focus_list_view {
-  padding: 20px;
 
-  .user_list {
-    display: grid;
-    grid-template-columns: repeat(5, 1fr);
-    grid-column-gap: 20px;
-    grid-row-gap: 20px;
-    width: 100%;
-
-
-    .item {
-      width: 100%;
-      display: flex;
-
-      .arco-avatar-circle {
-        flex-shrink: 0;
-      }
-
-      .info {
-        margin-left: 10px;
-
-        .abs {
-          height: 1.5rem;
-        }
-      }
-
-    }
-
-  }
-  .page{
-    display: flex;
-    justify-content: center;
-    margin-top: 20px;
-  }
-
-}
 </style>
