665128d0176a152d6874b8f198349ee28ef019ff
fengfeng <2663456523@qq.com>
2024-12-14 12:55:58 +0800

    全文搜索组件1

--- src/components/web/f_nav.vue
+++ src/components/web/f_nav.vue
@@ -1,14 +1,10 @@
 <script setup lang="ts">
-import {slogan, enSlogan} from "@/conf/global";
-import F_theme from "@/components/common/f_theme.vue";
-import {ref} from "vue";
-import F_user_dropdown from "@/components/common/f_user_dropdown.vue";
-
-import F_point from "@/components/common/f_point.vue";
+import {reactive, ref} from "vue";
 import F_nav_msg from "@/components/web/f_nav_msg.vue";
 import F_nav_avatar from "@/components/web/f_nav_avatar.vue";
-
-
+import type {listResponse, paramsType} from "@/api";
+import {textSearchApi, type textSearchType} from "@/api/search_api";
+import {Message} from "@arco-design/web-vue";
 
 interface Props {
   noScroll?: boolean
@@ -30,6 +26,23 @@ if (!noScroll) {
     }
   }
 }
+const visible = ref(true)
+const data = reactive<listResponse<textSearchType>>({
+  list: [],
+  count: 0
+})
+const form = reactive<paramsType>({
+  key: "",
+})
+
+async function search() {
+  const res = await textSearchApi(form)
+  if (res.code) {
+    Message.error(res.msg)
+    return
+  }
+  Object.assign(data, res.data)
+}
 
 
 </script>
@@ -43,9 +56,27 @@ if (!noScroll) {
           <span class="n2">社区版</span>
         </a>
       </div>
+      <a-modal title="全文搜索" :footer="false" body-class="f_article_search_modal_body scrollbar"
+               v-model:visible="visible">
+        <div class="head">
+          <a-input v-model="form.key" placeholder="请输入搜索内容"></a-input>
+          <a-button @click="search" type="primary">搜索</a-button>
+        </div>
+        <div class="body">
+          <div class="list">
+            <div class="item" v-for="item in data.list">
+              <div class="title" v-html="item.head"></div>
+              <div class="abs" v-html="item.body"></div>
+            </div>
+          </div>
+          <div class="page">
+            共搜索到结果条
+          </div>
+        </div>
+      </a-modal>
       <div class="center">
         <i class="iconfont icon-dengpao"></i>
-        <a-input-search placeholder="搜索你喜欢的文章"></a-input-search>
+        <a-input-search v-model="form.key" placeholder="搜索你喜欢的文章"></a-input-search>
       </div>
       <div class="right">
         <f_nav_avatar></f_nav_avatar>
@@ -171,4 +202,49 @@ if (!noScroll) {
   }
 }
 
+
+.f_article_search_modal_body {
+  padding: 0;
+  max-height: 70vh;
+  overflow-y: auto;
+
+  .head {
+    padding: 10px 20px;
+    display: flex;
+
+    .arco-btn {
+      margin-left: 10px;
+    }
+  }
+
+  .body {
+    .item {
+      padding: 10px 20px;
+      cursor: pointer;
+
+      .title {
+        font-size: 16px;
+      }
+
+      .abs {
+        margin-top: 5px;
+        font-size: 12px;
+      }
+
+      em {
+        color: red;
+      }
+
+      &:hover {
+        background-color: var(--color-fill-1);
+      }
+    }
+
+    .page {
+      padding: 10px 20px;
+      text-align: center;
+      color: var(--color-text-2);
+    }
+  }
+}
 </style>
